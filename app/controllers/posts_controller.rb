ffbefbbfa0f977c49c4396f83ea18d31e2f58bd4
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-03 06:24:46 +0700

    update def create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,12 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
