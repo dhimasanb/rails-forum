c369380cf1cae98ac52401bf16a6a344e24b9a8c
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-04 10:45:09 +0700

    tambah def destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,11 @@ class CommentsController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 end
