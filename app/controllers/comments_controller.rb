30dd139d8b43e46c3ee0d0cb64fcd31a00dce69f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-04 10:35:05 +0700

    tambah current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
+    @comment.user_id = current_user.id if current_user
 
     if @comment.save
       redirect_to post_path(@post)
